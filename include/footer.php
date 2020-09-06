97c0daef90de7dd060ac4b8ebf3704087e6e84c6
Aakarsh Teja <aakarshissa@gmail.com>
2020-09-06 23:54:46 +0530

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,61 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">About</h2>
+              <p>DataExtractor service is to provide micro services for the companies or the HR Team to extract the details like phone numbers and mails from large number of files</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="https://twitter.com/workassist_in"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+    <!--<li class="ftco-animate"><a href="#"><span class="icon-youtube"</span></a></li> -->
+    <li class="ftco-animate"><a href="https://www.linkedin.com/in/waytogo-consultants-a79aa8113"><span class="icon-linkedin"></span></a></li>
+    <li class="ftco-animate"><a href="#"><span class="icon-pinterest"></span></a></li>
+
+    
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-3">
+              <h2 class="ftco-heading-2">Employers</h2>
+              <ul class="list-unstyled">
+                <li><a href="signup" class="py-2 d-block">Register</a></li>
+                <!-- <li><a href="admin/admin_login.php" class="py-2 d-block">Post a Job</a></li> -->
+                <!-- <li><a href="#" class="py-2 d-block">Advance Skill Search</a></li> -->
+                <!-- <li><a href="#" class="py-2 d-block">Recruiting Service</a></li> -->
+                <li><a href="blog.php" class="py-2 d-block">Blog</a></li>
+                <!-- <li><a href="#" class="py-2 d-block">Faq</a></li> -->
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Have a Questions?</h2>
+              <div class="block-23 mb-3">
+                <ul>
+                  <!-- <li><span class="icon icon-map-marker"></span><span class="text"> Building, 224, AJC Bose Road, Kolkata  700017. West Bengal. India.</span></li> -->
+                  <li><a href=tel:+917702120928><span class="icon icon-phone"></span><span class="text">+91 7702120928<br></span></a></li>
+                  <li><a href="mailto:aakarshteja.i2017@vitstudent.ac.in"><span class="icon icon-envelope"></span><span class="text">aakarshteja.i2017@vitstudent.ac.in</span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to NN44 can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All Rights Reserved by DataExtractor</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'include/header.php'; ?>
+
+<div class="container">
+    <div>
+        <h1>Welcome to Data Extractor Service</h1>
+    </div>
+</div>
+
+<?php include 'include/footer.php'; ?>
