3a8f5146b8047a63b2e09ffaf018402aae371b96
Aakarsh Teja <aakarshissa@gmail.com>
2020-09-27 23:50:54 +0530

    support page modified

--- support.php
+++ support.php
@@ -1,9 +1,10 @@
 <?php include 'include/header.php'; ?>
 <div class="jumbotron">
         <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          <h1 class="display-3">Welcome to Data Extractor Support</h1>
+          <p>We as a quality service thrive to support our consumers.So please contact us through Mail.
+             Our mail Id is <strong>aaaa@aaaa.com</strong>. Or click on below link to redirect to your mail application.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Mail Us</a></p>
         </div>
 </div>
 <?php include 'include/footer.php'; ?>
